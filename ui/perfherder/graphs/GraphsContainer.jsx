6408090e1f945e9de2bb265f546de06788496c20
Sarah Clements <sclements313@gmail.com>
2019-07-24 15:35:08 -0700

    add jquery plot to graphsContainer

--- ui/perfherder/graphs/GraphsContainer.jsx
+++ ui/perfherder/graphs/GraphsContainer.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { Row } from 'reactstrap';
+import PropTypes from 'prop-types';
+import $ from 'jquery';
 
-const GraphsContainer = () => (
-  <React.Fragment>
-    <Row>
-      <div id="overview-plot" />
-    </Row>
-    <Row>
-      <div id="graph" />
-    </Row>
-  </React.Fragment>
-);
+const GraphsContainer = ({ testData }) => {
+  const plot = $.plot($('#graph'), testData.map(series => series.flotSeries), {
+    xaxis: { mode: 'time' },
+    series: { shadowSize: 0 },
+    selection: { mode: 'xy', color: '#97c6e5' },
+    lines: { show: false },
+    points: { show: true },
+    legend: { show: false },
+    grid: {
+      color: '#cdd6df',
+      borderWidth: 2,
+      backgroundColor: '#fff',
+      hoverable: true,
+      clickable: true,
+      autoHighlight: false,
+      // markings,
+    },
+  });
+  return (
+    <React.Fragment>
+      <Row>
+        <div id="overview-plot" />
+      </Row>
+      <Row>
+        <div id="graph" />
+      </Row>
+    </React.Fragment>
+  );
+};
+
+GraphsContainer.propTypes = {
+  testData: PropTypes.arrayOf(PropTypes.shape({})),
+};
+
+GraphsContainer.defaultProps = {
+  testData: [],
+};
 
 export default GraphsContainer;
