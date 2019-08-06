770faf05d04502d05c85634e2fcefc56cc0f7b1f
Sarah Clements <sclements313@gmail.com>
2019-08-05 17:11:26 -0700

    start replacing tooltipContent values

--- ui/perfherder/graphs/GraphTooltip.jsx
+++ ui/perfherder/graphs/GraphTooltip.jsx
@@ -7,10 +7,12 @@ import PropTypes from 'prop-types';
 // import { endpoints } from '../constants';
 // import PerfSeriesModel from '../../models/perfSeries';
 // import { thPerformanceBranches } from '../../helpers/constants';
+import countBy from 'lodash/countBy';
+
 import { displayNumber } from '../helpers';
 import { createQueryParams } from '../../helpers/url';
 
-export class GraphTooltip extends React.Component {
+export default class GraphTooltip extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -21,54 +23,113 @@ export class GraphTooltip extends React.Component {
     const signature = selectedDataPoint.datum
       ? selectedDataPoint.datum.signatureId
       : selectedDataPoint.signatureId;
-    console.log(signature);
+
     const testDetails = testData.find(item => item.signatureId === signature);
-    console.log(selectedDataPoint, testDetails);
+    const deltaValue = '';
+    const deltaPercentValue = '';
+
+    // var phSeries = $scope.seriesList.find(
+    //   s => s.signature_id === dataPoint.signatureId);
+
+    // we need the flot data for calculating values/deltas and to know where
+    // on the graph to position the tooltip
+    const flotIndex = testDetails.data.findIndex(item => item.pushId === selectedDataPoint.pushId);
+    // var flotData = {
+    //     series: $scope.plot.getData().find(
+    //         fs => fs.thSeries.signature_id === dataPoint.signatureId),
+    //     pointIndex: flotIndex,
+    // };
+    // check if there are any points belonging to earlier pushes in this
+    // graph -- if so, get the previous push so we can link to a pushlog
+
+    // Note this is the same number as flotIndex
+    const firstResultSetIndex = testDetails.resultSetData.indexOf(
+      selectedDataPoint.pushId,
+    );
+    const prevResultSetId =
+      firstResultSetIndex > 0
+        ? testDetails.resultSetData[firstResultSetIndex - 1]
+        : null;
+
+    const retriggerNum = countBy(testDetails.resultSetData, resultSetId =>
+      resultSetId === selectedDataPoint.pushId ? 'retrigger' : 'original',
+    );
+
+    console.log(firstResultSetIndex, prevResultSetId, retriggerNum, flotIndex)
+    const prevFlotDataPointIndex = (flotIndex - 1);
+    const flotSeriesData = testDetails.data;
+
+    // const t = flotSeriesData[flotIndex][0];
+    // const v = flotSeriesData[flotIndex][1];
+    // var v0 = (prevFlotDataPointIndex >= 0) ? flotSeriesData[prevFlotDataPointIndex][1] : v;
+    // var dv = v - v0;
+    // var dvp = v / v0 - 1;
+    // var alertSummary = phSeries.relatedAlertSummaries.find(alertSummary =>
+    //     alertSummary.push_id === dataPoint.resultSetId);
+    // var alert;
+    // if (alertSummary) {
+    //     alert = alertSummary.alerts.find(alert =>
+    //         alert.series_signature.id === phSeries.id);
+    // }
+    // const tooltipContent = {
+    //     project: $rootScope.repos.find(repo =>
+    //                 repo.name === phSeries.repository_name),
+    //     revisionUrl: `/#/jobs?repo=${phSeries.repository_name}`,
+    //     prevResultSetId: prevResultSetId,
+    //     resultSetId: dataPoint.resultSetId,
+    //     jobId: dataPoint.jobId,
+    //     series: phSeries,
+    //     value: Math.round(v * 1000) / 1000,
+    //     deltaValue: dv.toFixed(1),
+    //     deltaPercentValue: (100 * dvp).toFixed(1),
+    //     date: $.plot.formatDate(new Date(t), '%a %b %d, %H:%M:%S'),
+    //     retriggers: (retriggerNum.retrigger - 1),
+    //     alertSummary: alertSummary,
+    //     revisionInfoAvailable: true,
+    //     alert: alert,
+    // };
     return (
       <div className="body">
-        {/* <div>
-          <p id="tt-series">
-            {tooltipContent.series.test} ({tooltipContent.project.name})
-          </p>
+        <div>
+          <p id="tt-series">({testDetails.project})</p>
           <p id="tt-series2" className="small">
-            {tooltipContent.series.platform}
+            {testDetails.platform}
           </p>
         </div>
         <div>
           <p id="tt-v">
-            {displayNumber(tooltipContent.value)}
+            {displayNumber(selectedDataPoint.y)}
             <span className="text-muted">
-              {tooltipContent.series.lowerIsBetter
+              {testDetails.lowerIsBetter
                 ? ' (lower is better)'
                 : ' (higher is better)'}
             </span>
           </p>
           <p id="tt-dv" className="small">
-            &Delta; {displayNumber(tooltipContent.deltaValue)} (
-            {tooltipContent.deltaPercentValue}%)
+            &Delta; {displayNumber(deltaValue)} ({deltaPercentValue}%)
           </p>
         </div>
 
         <div>
-          {tooltipContent.revision && tooltipContent.prevRevision && (
+          {/* {testDetails.revision && testDetails.prevRevision && (
             <span>
               <a
                 id="tt-cset"
-                href={tooltipContent.pushlogURL}
+                href={testDetails.pushlogURL}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {tooltipContent.revision.slice(0, 13)}
+                {testDetails.revision.slice(0, 13)}
               </a> */}
-        {/* {tooltipContent.jobId && )
+          {/* {testDetails.jobId && )
                     (
                     <a
                       id="tt-cset"
                       href={`${getJobsUrl({
-                        repo: tooltipContent.project.name,
-                        revision: tooltipContent.revision,
+                        repo: testDetails.project.name,
+                        revision: testDetails.revision,
                       })}${createQueryParams({
-                        selectedJob: tooltipContent.jobId,
+                        selectedJob: testDetails.jobId,
                         group_state: 'expanded',
                       })}`}
                       target="_blank"
@@ -76,13 +137,13 @@ export class GraphTooltip extends React.Component {
                     >
                       job
                     </a>)} */}
-        {/* ,{' '}
+          {/* ,{' '}
               <a
                 href={`#/comparesubtest${createQueryParams({
-                  originalProject: tooltipContent.project.name,
-                  newProject: tooltipContent.project.name,
-                  originalRevision: tooltipContent.prevRevision,
-                  newRevision: tooltipContent.revision,
+                  originalProject: testDetails.project.name,
+                  newProject: testDetails.project.name,
+                  originalRevision: testDetails.prevRevision,
+                  newRevision: testDetails.revision,
                   originalSignature: selectedDataPoint.signatureId,
                   newSignature: selectedDataPoint.signatureId,
                   framework: selectedDataPoint.frameworkId,
@@ -95,29 +156,29 @@ export class GraphTooltip extends React.Component {
               )
             </span>
           )} */}
-        {/*
+          {/*
                 
               </p>
-              <p ng-if="tooltipContent.alertSummary">
+              <p ng-if="testDetails.alertSummary">
                 <i class="text-warning fas fa-exclamation-circle"></i>
-                <a href="perf.html#/alerts?id={{tooltipContent.alertSummary.id}}">
-                  Alert #{{tooltipContent.alertSummary.id}}</a>
-              <span class="text-muted">- {{tooltipContent.alert && (alertIsOfState(tooltipContent.alert, phAlertStatusMap.ACKNOWLEDGED) ? getAlertSummarytStatusText(tooltipContent.alertSummary) : getAlertStatusText(tooltipContent.alert))}}
-                  <span ng-show="tooltipContent.alert.related_summary_id">
-                    <span ng-if="tooltipContent.alert.related_summary_id !== tooltipContent.alertSummary.id">
-                    to <a href="#/alerts?id={{tooltipContent.alert.related_summary_id}}" target="_blank" rel="noopener">alert #{{tooltipContent.alert.related_summary_id}}</a>
+                <a href="perf.html#/alerts?id={{testDetails.alertSummary.id}}">
+                  Alert #{{testDetails.alertSummary.id}}</a>
+              <span class="text-muted">- {{testDetails.alert && (alertIsOfState(testDetails.alert, phAlertStatusMap.ACKNOWLEDGED) ? getAlertSummarytStatusText(testDetails.alertSummary) : getAlertStatusText(testDetails.alert))}}
+                  <span ng-show="testDetails.alert.related_summary_id">
+                    <span ng-if="testDetails.alert.related_summary_id !== testDetails.alertSummary.id">
+                    to <a href="#/alerts?id={{testDetails.alert.related_summary_id}}" target="_blank" rel="noopener">alert #{{testDetails.alert.related_summary_id}}</a>
                   </span>
-                  <span ng-if="tooltipContent.alert.related_summary_id === tooltipContent.alertSummary.id">
-                    from <a href="#/alerts?id={{tooltipContent.alert.related_summary_id}}" target="_blank" rel="noopener">alert #{{tooltipContent.alert.related_summary_id}}</a>
+                  <span ng-if="testDetails.alert.related_summary_id === testDetails.alertSummary.id">
+                    from <a href="#/alerts?id={{testDetails.alert.related_summary_id}}" target="_blank" rel="noopener">alert #{{testDetails.alert.related_summary_id}}</a>
                   </span>
                   </span>
                 </span>
               </p>
-              <p class="text-muted" ng-if="!tooltipContent.alertSummary">
+              <p class="text-muted" ng-if="!testDetails.alertSummary">
                 <span ng-if="!creatingAlert">
                   No alert
                   <span ng-if="user.isStaff">
-                    (<a href="" ng-click="createAlert(tooltipContent)" ng-disabled="user.isStaff">create</a>)
+                    (<a href="" ng-click="createAlert(testDetails)" ng-disabled="user.isStaff">create</a>)
                   </span>
                   <span ng-if="!user.isStaff">
                     (log in as a a sheriff to create)
@@ -127,13 +188,13 @@ export class GraphTooltip extends React.Component {
                   Creating alert... <i class="fas fa-spinner fa-pulse" title="creating alert"></i>
                 </span>
               </p>
-              <p ng-hide="tooltipContent.revision">
-                <span ng-hide="tooltipContent.revisionInfoAvailable">Revision info unavailable</span>
-                <span ng-show="tooltipContent.revisionInfoAvailable">Loading revision...</span>
+              <p ng-hide="testDetails.revision">
+                <span ng-hide="testDetails.revisionInfoAvailable">Revision info unavailable</span>
+                <span ng-show="testDetails.revisionInfoAvailable">Loading revision...</span>
               </p>
-              <p id="tt-t" class="small" ng-bind="tooltipContent.date"></p>
-              <p id="tt-v" class="small" ng-show="tooltipContent.retriggers > 0">Retriggers: {{tooltipContent.retriggers}}</p> */}
-        {/* </div> */}
+              <p id="tt-t" class="small" ng-bind="testDetails.date"></p>
+              <p id="tt-v" class="small" ng-show="testDetails.retriggers > 0">Retriggers: {{testDetails.retriggers}}</p> */}
+        </div>
       </div>
     );
   }
@@ -141,12 +202,9 @@ export class GraphTooltip extends React.Component {
 
 GraphTooltip.propTypes = {
   selectedDataPoint: PropTypes.shape({}),
-  tooltipContent: PropTypes.shape({}),
+  testData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 GraphTooltip.defaultProps = {
   selectedDataPoint: null,
-  tooltipContent: undefined,
 };
-
-export default GraphTooltip;
--- ui/perfherder/graphs/GraphsView.jsx
+++ ui/perfherder/graphs/GraphsView.jsx
@@ -216,6 +216,7 @@ class GraphsView extends React.Component {
           signatureId: series.signature_id,
           pushId: dataPoint.push_id,
         })),
+        lowerIsBetter: series.lower_is_better,
         // TODO Are these needed?
         resultSetData: series.data.map(dataPoint => dataPoint.push_id),
         jobIdData: series.data.map(dataPoint => dataPoint.job_id),
