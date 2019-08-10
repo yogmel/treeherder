0709d1aa05eb28bb757adac598669cee2b6e59e8
Sarah Clements <sclements313@gmail.com>
2019-08-10 12:06:41 -0700

    add mock data

--- tests/ui/perfherder/graphs_view_test.jsx
+++ tests/ui/perfherder/graphs_view_test.jsx
@@ -10,8 +10,20 @@ import {
 
 import GraphsViewControls from '../../../ui/perfherder/graphs/GraphsViewControls';
 import TestDataModal from '../../../ui/perfherder/graphs/TestDataModal';
-import optionCollectionMap from '../mock/optionCollectionMap';
-import { summaryStatusMap } from '../../../ui/perfherder/constants';
+// import { summaryStatusMap } from '../../../ui/perfherder/constants';
+import repos from '../mock/repositories';
+import testData from '../mock/performance_summary.json';
+
+const frameworks = [
+  { id: 1, name: 'talos' },
+  { id: 2, name: 'build_metrics' },
+  { id: 3, name: 'autophone' },
+];
+
+let showModal = false;
+function toggle() {
+  showModal = !showModal;
+}
 
 const graphsViewControls = () =>
   render(
@@ -28,11 +40,11 @@ const graphsViewControls = () =>
         <TestDataModal
           showModal={showModal}
           frameworks={frameworks}
-          projects={projects}
-          timeRange={timeRange.value}
-          options={options}
-          getTestData={this.getTestData}
-          toggle={() => this.toggle('showModal')}
+          projects={repos}
+          timeRange={172800}
+          options={{}}
+          getTestData={() => {}}
+          toggle={toggle}
           testData={testData}
         />
       }
